6f6fc275904e98b95f3ad9905183ce71187934fa
Rohit Maurya <rohit@cybersify.tech>
2024-05-29 17:35:27 +0530

    add contact

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -16,7 +16,6 @@ import adminSlice from '../redux-setup/adminSlice'
 import statusSlice from '../redux-setup/orderStatus'
 import sidebarSlice from '../redux-setup/sidebarSlice'
 import socketSlice from '../redux-setup/socketSlice'
-import mainReducer from './features/mainSlice'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { StrictMode, useEffect, useState } from 'react'
@@ -57,7 +56,6 @@ const combinedReducer = combineReducers({
   orders: ordersSlice,
   socket: socketSlice,
   sidebar: sidebarSlice,
-  main: mainReducer,
   userOrder: statusSlice,
   page: pageSlice,
 })
@@ -76,44 +74,37 @@ const store = configureStore({
 })
 
 export default function MyApp({ Component, pageProps }) {
-
-  
   const options = {
     // passing the client secret obtained from the server
     clientSecret:
       'sk_test_51OzXu4SG76jQkcdqbcqN5THTrNz2CjxTmZHDHfkCUClt3PjwMHn8Mr99Mkg1JT6GhcOQunAhRXNxJf9a24nrUoH300M9QrbB4J',
   }
- 
+
   return (
+    // <Elements
+    //   stripe={stripePromise}
+    //   // options={options}
+    // >
     <>
       {/* <DefaultSeo
-    title="Ethical Swag | North America | Sustainable Promotional Products"
-    description="Ethical Swag provides a suite of turn-key services designed with convenience in mind to streamline your swag management process and elevate your brand presence."
-    openGraph={{
-      type: 'website',
-      locale: 'en-ca',
-      url: 'https://ethicalswag.com/',
-      siteName: 'Ethical Swag',
-      images: [
-        {
-          url: 'https://ethical-images.s3.ca-central-1.amazonaws.com/hero_home_section_image.jpg',
-          width: 1200,
-          height: 630,
-          alt: 'Ethical Swag - Sustainable Promotional Products',
-        },
-      ],
-    }}
-    twitter={{
-      handle: '@ethicalswag',
-      site: '@ethicalswag',
-      cardType: 'summary_large_image',
-    }}
-  /> */}
-     
+        title="Ethical Swag | North America | Sustainable Promotional Products"
+        description="Ethical Swag provides a suite of turn-key services designed with convenience in mind to streamline your swag management process and elevate your brand presence."
+        openGraph={{
+          type: 'website',
+          locale: 'en-ca',
+          url: 'https://ethicalswag.com/',
+          siteName: 'Ethical Swag',
+        }}
+        twitter={{
+          handle: '@ethicalswag',
+          site: '@ethicalswag',
+          cardType: 'summary_large_image',
+        }}
+      /> */}
+       <DefaultSeo {...SEO} />
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistStore(store)}>
           <StrictMode>
-          <DefaultSeo {...SEO} />
             <Component {...pageProps} suppressHydrationWarning />
 
             <ToastContainer />
